e6977312ddbcf3ddb735b4bcd092351cb8c2aa50
LisianeAguiar <lisianeag@hotmail.com>
2017-12-05 20:53:08 -0200

    novas funções de update e lookup

--- src/utils.ml
+++ src/utils.ml
@@ -13,15 +13,29 @@ let rec type2string t = match t with
     | TyBool -> "bool"
     | TyFn(t1,t2) -> let t1' = type2string(t1) in let t2' = type2string(t2) in "fun("^t1'^"->"^t2'^")" ;;
 
-(* Function to update environment *)
+(* Function to update environment. Arguments: variable, value, env *)
 let update variable value environment : env = match environment with
   | [] -> [(variable, value)]
   | hd::tl -> List.append [(variable, value)] environment ;;
 
-(* Function to look up for variable in environment *)
+(* Function to update environment. Arguments: variable, expr, env*)
+
+let updateExpr variable expr environment : envExp = match environment with
+  | [] -> [(variable, expr)]
+  | hd::tl -> List.append [(variable, expr)] environment ;;
+  
+(* Function to look up for variable in environment. Return a value  *)
 let rec lookup variable environment : value = match environment with
   | [] -> raise Variable_not_found
   | (name, v)::tl ->
     if (name == variable)    (* Found the variable in the head *)
     then v                  (* Returns variable value *)
     else lookup variable tl ;; (* Look for it in the tale *)
+    
+   (* Function to look up for variable in environment. Return a expr  *)
+let rec lookupExpr variable environment : expr = match environment with
+  | [] -> raise Variable_not_found
+  | (name, v)::tl ->
+    if (name == variable)    (* Found the variable in the head *)
+    then v                  (* Returns variable value *)
+    else lookupExpr variable tl ;; (* Look for it in the tale *)
